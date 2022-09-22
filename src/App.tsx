79aeb6b09d8b868c72c61bb3a89e4da68066da5d
reverb6821 <giuseppegigliottidev@gmail.com>
2022-09-22 10:12:37 +0200

    creted custom context

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { IMovie } from './model/IMovie';
+import { discoverMovies } from './services/movieService';
+import { MoviesContext } from './services/context';
 
 function App() {
+
+  useEffect(() => {
+    discoverMovies()
+      .then(setMovies)
+      .catch((_) => setMovies([]));
+  }, []);
+
+  const [movies, setMovies] = useState<IMovie[]>([]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MoviesContext.Provider value={{ movies, updateMovies: setMovies }}>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </MoviesContext.Provider>
   );
 }
 
